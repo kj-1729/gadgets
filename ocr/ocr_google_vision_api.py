7a3e38bae42b8f9cb300da61b077307453082096
kj-1729 <87534698+kj-1729@users.noreply.github.com>
2023-03-22 00:18:29 +0900

    Update and rename ocr_google-vision-api.py to ocr_google_vision_api.py

--- ocr/ocr_google_vision_api.py
+++ ocr/ocr_google_vision_api.py
@@ -12,14 +12,18 @@ class CFG:
 	idx_seqno = 0
 	idx_fullpath = 7
 	idx_dir = 6
+
+	root_dir = r'C:\Users\Koji\Documents\programs\python\devel\ocr'
 	credentials_fname = '(credential_file_name).json'
+
 	
 class ocr:
 	def __init__(self):
-		credentials = service_account.Credentials.from_service_account_file(CFG.credentials_fname)
+		credentials_fullpath = os.path.join(CFG.root_dir, CFG.credentials_fname)
+		credentials = service_account.Credentials.from_service_account_file(credentials_fullpath)
 		self.client = vision.ImageAnnotatorClient(credentials=credentials)
 
-	def do_ocr(self, image_fullpath, text_fullpath):
+	def img2txt(self, image_fullpath):
 		print(f'image: {image_fullpath}')
 		with open(image_fullpath, 'rb') as fp:
 		    content = fp.read()
@@ -27,10 +31,14 @@ class ocr:
 		image = vision.Image(content=content)
 		response = self.client.document_text_detection(image=image)
 		
-		txt = response.full_text_annotation.text
+		self.txt = response.full_text_annotation.text
 		#txt_encoded = txt.encode('cp932', 'ignore')
+
+		return self.txt
+
+	def save_txt(text_fullpath):
 		with open(text_fullpath, 'w', encoding='utf-8') as fp:
-			fp.write(txt)
+			fp.write(self.txt)
 
 def main():
 	ocr_hd = ocr()
@@ -41,7 +49,8 @@ def main():
 		data = line[:-1].split('\t')
 		img_fullpath = data[CFG.idx_fullpath]
 		txt_fullpath = os.path.join(data[CFG.idx_dir], data[CFG.idx_seqno] + '.txt')
-		ocr_hd.do_ocr(img_fullpath, txt_fullpath)
+		ocr_hd.img2txt(img_fullpath)
+		ocr_hd.save_text(txt_fullpath)
 
 if __name__ == '__main__':
 	main()
