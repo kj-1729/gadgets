43bf5246152c6d9d4860b87981964d4fef103764
kj-1729 <87534698+kj-1729@users.noreply.github.com>
2023-04-29 12:29:04 +0900

    Update xls_copy_and_paste.py

--- ETL/xls_copy_and_paste.py
+++ ETL/xls_copy_and_paste.py
@@ -7,16 +7,6 @@ def process(sheet_config, wb_from, wb_to):
 	
 	idx_row = 9
 	while flg > 0:
-		sheet_from = sheet_config.cell(row=idx_row, column=1).value
-		row_from = sheet_config.cell(row=idx_row, column=2).value
-		col_from = sheet_config.cell(row=idx_row, column=3).value
-		sheet_to = sheet_config.cell(row=idx_row, column=4).value
-		row_to = sheet_config.cell(row=idx_row, column=5).value
-		col_to = sheet_config.cell(row=idx_row, column=6).value
-		
-		wb_to[sheet_to].cell(row=row_to, column=col_to).value = wb_from[sheet_from].cell(row=row_from, column=col_from).value
-
-		idx_row += 1
 		if sheet_config.cell(row=idx_row, column=1).value is None:
 			flg = 0
 		elif len(sheet_config.cell(row=idx_row, column=1).value) == 0:
@@ -24,20 +14,35 @@ def process(sheet_config, wb_from, wb_to):
 		elif idx_row > 100000:
 			flg = 0
 
+		if flg > 0:
+			sheet_from = sheet_config.cell(row=idx_row, column=1).value
+			row_from = sheet_config.cell(row=idx_row, column=2).value
+			col_from = sheet_config.cell(row=idx_row, column=3).value
+			sheet_to = sheet_config.cell(row=idx_row, column=4).value
+			row_to = sheet_config.cell(row=idx_row, column=5).value
+			col_to = sheet_config.cell(row=idx_row, column=6).value
+			
+			wb_to[sheet_to].cell(row=row_to, column=col_to).value = wb_from[sheet_from].cell(row=row_from, column=col_from).value
+
+		idx_row += 1
+
 def main():
-	if len(sys.argv) < 2:
-		sys.stderr.write('Usage: copy_and_paste.py config_file\n')
+	if len(sys.argv) < 3:
+		sys.stderr.write('Usage: copy_and_paste.py config_file config_sheet\n')
 		sys.exit(1)
 	
 	config_path = sys.argv[1]
+	config_sheet = sys.argv[2]
 	
 	# get input/output excel file
 	wb_config = openpyxl.load_workbook(config_path, data_only=True)
-	sheet_config = wb_config['config']
+	sheet_config = wb_config[config_sheet]
+
 	dir_from = sheet_config.cell(row=2, column=4).value
 	fname_from = sheet_config.cell(row=3, column=4).value
 	dir_to = sheet_config.cell(row=4, column=4).value
 	fname_to = sheet_config.cell(row=5, column=4).value
+
 	path_from = os.path.join(dir_from, fname_from)
 	path_to = os.path.join(dir_to, fname_to)
 	
